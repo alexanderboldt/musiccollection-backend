d5eeaf5d00d9c7fa1197505e19410e3d7de1ce09
Alexander Boldt <boldt.alexander@outlook.com>
2026-03-25 15:02:05 +0100

    improved aws cognito object

--- opentofu/cognito.tf
+++ opentofu/cognito.tf
@@ -2,21 +2,27 @@ resource "aws_cognito_user_pool" "main" {
   name = "musiccollection-user-pool"
 
   auto_verified_attributes = ["email"]
-
-  password_policy {
-    minimum_length = 8
-  }
 }
 
 resource "aws_cognito_user_pool_client" "client" {
   name            = "musiccollection-client"
   user_pool_id    = aws_cognito_user_pool.main.id
-  generate_secret = false
+  generate_secret = true
+
+  allowed_oauth_flows                   = ["code"]
+  allowed_oauth_scopes                  = ["openid", "profile", "email"]
+  supported_identity_providers          = ["COGNITO"]
+  allowed_oauth_flows_user_pool_client  = true
+  callback_urls                         = ["https://d84l1y8p4kdic.cloudfront.net"]
 
   explicit_auth_flows = [
     "ALLOW_USER_AUTH",
     "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
+}
+
+resource "aws_cognito_user_pool_domain" "domain" {
+  domain       = "musiccollection-auth-domain"
+  user_pool_id = aws_cognito_user_pool.main.id
 }
