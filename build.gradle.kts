5b28b62a806531e0bccd67567d0baa4a3bf68f75
Alexander Boldt <boldt.alexander@outlook.com>
2026-05-18 11:15:26 +0200

    removed minio-testcontainer dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
 
     testImplementation(libs.testcontainers.junit.jupiter)
     testImplementation(libs.testcontainers.postgresql)
-    testImplementation(libs.testcontainers.minio)
 }
 
 group = "org.musiccollection"
